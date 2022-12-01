6b3815f7c893c1021d5dd6b9fa74f243fc73e372
immyprince <immyprince2000@gmail.com>
2022-11-30 20:05:44 -0500

    Merged some changes.
    Removed main page and other page routes. Drop down menu added for selecting other player card styles.

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
 import CharacterInput from './components/CharacterInput';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import CharacterCard from './components/CharacterCard';
-import Main from './Main';
 
 function App() {
   return (
     <div className="App">
-      <Main/>
-      <BrowserRouter>
-      <Routes>
-          <Route path="/create" element= {<CharacterInput />} />
-          <Route path="/result" element= {<CharacterCard />} />
-      </Routes>
-    </BrowserRouter>
+      <CharacterInput/>
     </div>
   );
 }
--- app/src/components/CharacterCard.js
+++ app/src/components/CharacterCard.js
@@ -1,27 +1,58 @@
 import './CharacterCard.css';
 import React, { useState, useEffect } from 'react'
+import Select from 'react-select';
 
 export default function CharacterCard(props) {
     const [stylePath, setState] = useState({
-        style: "a.css"
+        style: ""
     });
+
+    const optionList = [
+        { value: "b.css", label: "Square" },
+        { value: "a.css", label: "Circle" }
+    ];
 
     useEffect(() => {
         var head = document.head;
         var link = document.createElement("link");
-    
+
         link.type = "text/css";
         link.rel = "stylesheet";
         link.href = stylePath.style;
-    
+
         head.appendChild(link);
-    
+
         return () => { head.removeChild(link); }
-    
+
     }, [stylePath]);
 
-    function onChange() {
-        setState({...stylePath, style: "b.css"})
+    function handleSelect(data) {
+        setState({ ...stylePath, style: data.value })
+    }
+
+    function downloadJSON() {
+        let json = {
+            Name: props.name,
+            Class: props.class,
+            Strength: props.strength,
+            Dexterity: props.dexterity,
+            Constitution: props.constitution,
+            Intelligence: props.intelligence,
+            Wisdom: props.wisdom,
+            Charisma: props.charisma
+        }
+
+        const blob = new Blob([JSON.stringify(json)], {type:'text/json'})
+        const a = document.createElement('a')
+        a.download = json.Name + '-character-card.json'
+        a.href = window.URL.createObjectURL(blob)
+        const clickEvent = new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true
+        })
+        a.dispatchEvent(clickEvent)
+        a.remove()
     }
 
     function downloadJSON() {
@@ -50,22 +81,37 @@ export default function CharacterCard(props) {
     }
 
     return (
-        <div className='card'>
-            <button onClick={onChange}>Button</button>
+        <div>
+            <div className="dropdown-container">
+                <Select
+                    options={optionList}
+                    placeholder="Select color"
+                    value={stylePath}
+                    label="Theme"
+                    onChange={handleSelect}
+                    isSearchable={true}
+                />
+            </div>
+            <div className='card'>
+
             <button onClick={downloadJSON}>Download JSON</button>
-            <div id='portrait'>
-                <img src={props.url} alt="" width='300' height='400'></img>
+                <div id='portrait'>
+                    <img src={props.url} alt="" width='300' height='400'></img>
+                </div>
+                <div className='stats'>
+                    <h1>Name: {props.name}</h1>
+                    <h1>Class: {props.class}</h1>
+                    <h1>Strength: {props.strength}</h1>
+                    <h1>Dexterity: {props.dexterity}</h1>
+                    <h1>Constitution: {props.constitution}</h1>
+                    <h1>Intelligence: {props.intelligence}</h1>
+                    <h1>Wisdom: {props.wisdom}</h1>
+                    <h1>Charisma: {props.charisma}</h1>
+                </div>
             </div>
-            <div className='stats'>
-                <h1>Name: {props.name}</h1>
-                <h1>Class: {props.class}</h1>
-                <h1>Strength: {props.strength}</h1>
-                <h1>Dexterity: {props.dexterity}</h1>
-                <h1>Constitution: {props.constitution}</h1>
-                <h1>Intelligence: {props.intelligence}</h1>
-                <h1>Wisdom: {props.wisdom}</h1>
-                <h1>Charisma: {props.charisma}</h1>
-            </div>
+            <form>
+                <input type="submit" value="Create New Character" /><br />
+            </form>
         </div>
     );
 }
